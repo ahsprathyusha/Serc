a5a4eb66107e1df2146fc18d0b8b167bb63f893c
Hema <prathyusha.ahs@gmail.com>
2022-07-08 14:32:06 +0100

    Added Taniascript

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ app.appendChild(logo)
 app.appendChild(container)
 
 var request = new XMLHttpRequest()
-request.open('GET', 'https://petstore.swagger.io/', true)
+request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
 request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
